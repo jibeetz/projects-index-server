f25ba933ae8feac7a5ecfc50bb27f9810aea3e5d
Jean-Baptiste Bernus <jbernus@gmail.com>
2018-05-30 22:46:52 +0200

    Refactor scraper, prepare for loop

--- scraper.js
+++ scraper.js
@@ -4,146 +4,75 @@ const puppeteer = require('puppeteer');
 
     // let firstName = process.env.firstName;
 
-    // async function TakeScreenshot(name) {
-    //     var currentDate = (new Date).getDay() + "d" + (new Date).getMonth() + "m" + (new Date).getFullYear() + "T" + (new Date).getHours() + "h" + (new Date).getMinutes() + "m" + (new Date).getSeconds() + 's'
-    //     await page.screenshot({ path: 'test/screenshots/' + currentDate + '_' + name + '.png', fullPage: true });
-    // }
-	// https://www.mlb.com/scores/2018-05-26
 
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({headless: false, devtools: true, args: ['--start-maximized']});
 	const page = await browser.newPage();
 
-	// await page.goto('http://chezrevel.net');
+	async function captureTeam(page, team) {
 
-	// let gamesList = await page.evaluate(() => {
-	// 	let list = document.querySelectorAll('#hp > div.hp-list.docs > ul > li');
+		await page.goto('https://www.baseball-reference.com/teams/' + team +'/2018-schedule-scores.shtml', {waitUntil: 'networkidle2'});
 
-	// 	let data = [];
-	// 	for (const item of list) {
-	// 		let title = item.childNodes[0].innerText; // Select the title
+		let gamesList = await page.evaluate((team) => {
 
-	// 		data.push({title});
-	// 	}
+			let team_nice_name = document.querySelector('#meta > div:nth-child(2) > h1 > span:nth-child(2)').innerText;
+			let year = document.querySelector('#meta > div:nth-child(2) > h1 > span:nth-child(1)').innerText;
+			let league = document.querySelector('#meta > div:nth-child(2) > p:nth-child(3) > a:nth-child(2)').innerText;
 
-	// 	return data;
+			let teamData = {
+				meta: {
+					team: team,
+					league: league,
+					year: year,
+					team_nice_name: team_nice_name
+				}
+			};
 
-	// });
+			let list = document.querySelectorAll('#team_schedule > tbody > tr:not([class="thead"]');
 
-	// console.log('gamesList', gamesList);
+			let data = [];
 
-	await page.goto('https://www.baseball-reference.com/teams/CLE/2018-schedule-scores.shtml');
+			const cleanResult = (result) => {
+				if(result === 'L-wo')
+					return 'L';
 
-	let gamesList = await page.evaluate(() => {
-		let list = document.querySelectorAll('#team_schedule > tbody > tr:not([class="thead"]');
+				if(result === 'W-wo')
+					return 'W';
 
-		let data = [];
-
-		for (const liste of list) {
-			if(liste.childNodes[2].innerText === 'boxscore'){
-				let game = liste.childNodes[0].innerText;
-				let result = liste.childNodes[6].innerText;
-
-				data.push({game: game, result: result});
+				return result;
 			}
 
-		}
+			for (const liste of list) {
+				if(liste.childNodes[2].innerText === 'boxscore'){
+					let game = liste.childNodes[0].innerText;
+					let result = liste.childNodes[6].innerText;
 
+					result = cleanResult(result)
 
-		return data;
+					data.push({game: game, result: result});
+				}
 
-	});
+			}
 
-	console.log('gamesList', gamesList);
+			teamData.scores = data;
 
+			console.log('uhuhuh');
 
-    // ---------------------------------------------------------
+			debugger;
 
+			return teamData;
 
-	// await page.waitFor(3000);
+		}, team);
 
-	// await page.goto('https://www.mlb.com/scores/2018-05-26');
+		console.log('gamesList', await gamesList);
 
-	// let gamesList = await page.evaluate(() => {
-	// 	let gamesContainer = document.querySelectorAll('.mlb-scores__list.mlb-scores__list--games')[0];
+		await page.waitFor(3000);
+	}
 
-	// 	let gamesEls = gamesContainer.querySelectorAll('li');
+	const teams = ['CLE', 'BOS', 'NYY']
 
-	// 	console.log(await gamesEls);
-
-	// 	for (const gameEl of gamesEls) {
-
-	// 	}
-
-	// 	const gameTeams = []
-	// 	for (const gameEl of gamesEls) {
-
-	// 		let teamAway = gameEl.querySelectorAll('.g5-component--mlb-scores__linescore__table__team-row.g5-component--mlb-scores__linescore__table__team-row--away')[0];
-
-	// 		teamAway.querySelector('.g5-component--mlb-scores__team__info__name--abbrev').textContent;
-	// 		console.log('teamAway', teamAway);
-	// 		gameTeams.push(teamAway);
-
-
-	// 	}
-
-	// 	return gamesEls;
-	// });
-
-	// console.log('gamesList', gamesList);
-
-
-
-	// let teams = await gamesList.map(function(gameBody) {
-		//console.log('gameBody', gameBody);
-
-		// teams.push(gameBody);
-
-		// let searchValue = '';
-		// let teamsEls = gameBody.querySelectorAll('.g5-component--mlb-scores__linescore__table__team-row')
-
-		// teams.push(i);
-		// for (const teamEl of teamsEls) {
-		// 	let didTeamLoose = teamEl.querySelector('.td g5-component--mlb-scores__team').classList.contains('g5-component--mlb-scores__team--loser')
-
-		// 	// teams.push(
-		// 	// 	{
-		// 	// 		date: 'date',
-
-		// 	// 	}
-		// 	// )
-		// }
-
-
-
-		// return searchValue;
-	// });
-
-	//console.log('teams', teams);
-
-	// for (let i = 1; i <= listLength; i++) {
-
-	// 	console.log('i', i);
-	// 	// change the index to the next child
-	// 	// let usernameSelector = LIST_USERNAME_SELECTOR.replace("INDEX", i);
-	// 	// let emailSelector = LIST_EMAIL_SELECTOR.replace("INDEX", i);
-
-	// 	// let username = await page.evaluate((sel) => {
-	// 	// 	return document.querySelector(sel).getAttribute('href').replace('/', '');
-	// 	//   }, usernameSelector);
-
-	// 	// let email = await page.evaluate((sel) => {
-	// 	// 	let element = document.querySelector(sel);
-	// 	// 	return element? element.innerHTML: null;
-	// 	//   }, emailSelector);
-
-	// 	// // not all users have emails visible
-	// 	// if (!email)
-	// 	//   continue;
-
-	// 	// console.log(username, ' -> ', email);
-
-	// 	// TODO save this user
-	// }
+	for (let team of teams) {
+		await captureTeam(page, team);
+	}
 
 	await browser.close();
 })();
